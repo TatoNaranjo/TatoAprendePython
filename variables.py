bb0b1c175a4c9e0b2b67886e17dd56b403dbfbe7
Tato <naranjosa2004@gmail.com>
2022-10-08 10:24:30 -0500

    Tipos de Variables en Python

--- variables.py
+++ variables.py
@@ -7,9 +7,9 @@ print("Este es un numero Entero:",entero)
 #Este es un numero Decimal
 decimal = 5.5
 print("Este es un Numero Decimal: ",decimal)
-#Este es un Numero Imaginario
-imaginario = 5j
-print("Este es un Numero Decimal: ",imaginario)
+#Este es un Numero Complejo
+complejo = 5j
+print("Este es un Numero complejo: ", complejo)
 #Este es un Texto Escrito con Comillas Dobles
 texto = "Este es un Texto"
 print("Este es un Texto Normal: ",texto)
