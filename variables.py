bbe33ffbc199dd3b760a31900badd3837a8976a0
Tato <naranjosa2004@gmail.com>
2022-10-08 10:00:30 -0500

    Tipos de Variables en Python

---
+++ variables.py
@@ -0,0 +1,20 @@
+#Aprendiendo Variables
+
+#Este es un numero Entero
+entero = 5
+print("Este es un numero Entero:",entero)
+#Este es un numero Decimal
+decimal = 5.5
+print("Este es un Numero Decimal: ",decimal)
+#Este es un Numero Imaginario
+imaginario = 5j
+print("Este es un Numero Decimal: ",imaginario)
+#Este es un Texto Escrito con Comillas Dobles
+texto = "Este es un Texto"
+print("Este es un Texto Normal: ",texto)
+#Este es un Texto Escrito con Comillas Simples
+textoComillas = 'Este es un Texto con Comillas Simples'
+print("Este es un Texto con Comillas Simples",textoComillas)
+
+negativo = -54
+print(abs(negativo))
