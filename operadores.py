2efa37cf195b50013172946cf5d0528603238d88
mi nombre <naranjosa2004@gmail.com>
2022-10-16 10:33:27 -0500

    Operadores en Python

---
+++ operadores.py
@@ -0,0 +1,40 @@
+"""
+a = int (input("Ingresa El Numero Uno :D "))
+b = int (input("Ingresa el Numero Dos :D "))
+c = int (a+b)
+print(c)
+"""
+#Operaciones con Numeros
+
+#Suma
+print(3+4)
+#Resta
+print(3-4)
+#Multiplicacion
+print(3*4)
+#Division
+print(3/4)
+#Modulo
+print(10%3)
+#Division Aproximada
+print(10//3)
+#Potencia
+print(2**3)
+
+#Operaciones con Strings
+
+print("Hola "+"Python") #Concatenando Strings
+
+#Posibles Errores.
+"""
+Print("Hola"-"Python") 
+Esto totearía el código xd
+
+print ("Hola"+5)
+No se puede mezclar un String con un Int
+"""
+#Fin de los Posibles errores xd
+print("Hola "+str(5)) # Esto si funciona porque pasamos el 5 a string
+
+print("Hola "*5)# Hola se Multiplica 5 veces, es decir se imprime 5 veces
+
