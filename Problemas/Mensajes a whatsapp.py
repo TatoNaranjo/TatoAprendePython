85e8e21c643fa116017de8b1c4caca42379f700b
mi nombre <naranjosa2004@gmail.com>
2022-10-18 07:23:02 -0500

    Problemas Resueltos en Python

---
+++ Problemas/Mensajes a whatsapp.py
@@ -0,0 +1,12 @@
+# Importamos el ModuMódulo
+import pywhatkit
+# Usamos Un try-except
+try:
+  # Enviamos el mensaje
+  pywhatkit.sendwhatmsg("+573212861162",  
+                        "Hola Daniel, este es un mensaje mandado por un Bot de Python",
+                        7,18)
+  print("Mensaje Enviado")
+  
+except:
+  print("Ocurrio Un Error")
