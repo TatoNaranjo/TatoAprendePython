1126846060cd6b83bc4bc0939b2c0714002b6a08
mi nombre <naranjosa2004@gmail.com>
2022-10-23 15:24:29 -0500

    Como Leer Lineas dadas en Plataformas de Competicion.

---
+++ leerStringsCodeforces.py
@@ -0,0 +1,11 @@
+"""
+Normalmente, leer unos parametros que nos piden en Codeforces sería facil.
+Pero en Python la cosa se complica un poco más. Es Por eso que me he tomado
+La tarea de documentar como leemos los inputs de estas Plataformas de Competición.
+
+"""
+
+# Ejemplo de Input: 2 3 5 3 1
+listas = map(int,input().split())
+listas = list(listas)
+print(listas)
